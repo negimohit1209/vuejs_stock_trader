068ec2c84bdf7212839bc43b06c948cd74cd6aa8
negimohit1209 <mohitsngh1997@gmail.com>
2019-10-31 00:03:54 +0530

    adds stock card and logic for rendering stock

--- src/components/Stocks/Stock.vue
+++ src/components/Stocks/Stock.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>stocks - stock component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    NAME
+                    <small>(price: Price)</small>
+                </h3>
+            </div>
+            <div class="div panel-body">
+                <div class="div panel-left">
+                    <input 
+                    type="number"
+                    class="form-control"
+                    placeholder="Quantity">
+                </div>
+                <div class="panel-right">
+                    <button class='btn btn-success'>Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- src/components/Stocks/Stocks.vue
+++ src/components/Stocks/Stocks.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>Stocks component</h1>
+    <div>
+        <app-stock v-for="(stock, index) in stocks" :key=index></app-stock>
+    </div>
 </template>
+
+<script>
+import Stock from './Stock.vue'
+export default {
+    data(){
+        return {
+            stocks: [
+                {id: 1, name: 'bmw', price: 110},
+                {id: 2, name: 'Google', price: 200},
+                {id: 3, name: 'Apple', price: 250},
+                {id: 4, name: 'Twitter', price: 80},
+            ]
+        }
+    },
+    components: {
+        appStock: Stock
+    }
+}
+</script>
